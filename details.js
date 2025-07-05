81d6ba73662e190b1557e894fd093895415c1bb9
MaAs0706e <aswanthmadhav07@gmail.com>
2025-07-05 16:01:46 +0530

    feat : added the timings of the commit messages

--- details.js
+++ details.js
@@ -2,6 +2,7 @@ const params = new URLSearchParams(window.location.search);
 const owner = params.get('owner');
 const repo = params.get('repo');
 
+// fetch readme
 
 document.getElementById('project-title').textContent = repo;
 fetch(`https://api.github.com/repos/${owner}/${repo}/readme`, {
@@ -19,16 +20,29 @@ fetch(`https://api.github.com/repos/${owner}/${repo}/readme`, {
     console.error(err);
   });
 
-  fetch(`https://api.github.com/repos/${owner}/${repo}/commits`)
-  .then(res => res.json())
-  .then(commits => {
+// Fetch commits
+
+fetch(`https://api.github.com/repos/${owner}/${repo}/commits`)
+.then(res => res.json())
+.then(commits => {
     const commitsList = document.getElementById('commits');
     commits.forEach(commit => {
-       const li = document.createElement('li');
-       li.textContent = commit.commit.message;
-       commitsList.appendChild(li);
-    });
-  })
+  const li = document.createElement('li');
+  
+  const message = commit.commit.message;
+  const date = new Date(commit.commit.author.date).toLocaleString();
+
+  li.innerHTML = `
+    <div class="commit-item">
+      <div class="commit-message">${message}</div>
+      <div class="commit-date">${date}</div>
+    </div>
+  `;
+
+  commitsList.appendChild(li);
+});
+
+})
   .catch(err => {
     document.getElementById('commits').textContent = 'Could not load commits.';
     console.error(err);
